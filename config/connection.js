4722ce4864b31e7425c739e87e501a3cf54a1610
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-12-11 08:42:54 -0800

    changing a few lines in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,12 @@ const  mysql = require( "mysql");
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
-
-connection.end();
 
 module.exports = connection;
