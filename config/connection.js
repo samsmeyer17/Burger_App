980bfa7d79fbb1b0061c37b916d745a9dff94dfa
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-12-11 07:33:37 -0800

    tyring to configure the database connection for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,15 @@
 const  mysql = require( "mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mey24875",
-  database: "burgers_db"
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.end();
 
 module.exports = connection;
