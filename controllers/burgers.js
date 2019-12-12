9a7251acfcd5834264d440464d4fb92a4461927e
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-12-12 11:25:30 -0800

    adding '/' route to get to index.html

--- controllers/burgers.js
+++ controllers/burgers.js
@@ -4,7 +4,11 @@ var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
-console.log("API ROUTES LOADED")
+console.log("API ROUTES LOADED");
+
+router.get('/', function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"))
+});
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
